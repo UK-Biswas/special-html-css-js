1f10e5f050ed0e4a04059b2b03711546e0e8440a
UK-Biswas <uzzwalbiswas70@gmail.com>
2026-08-13 20:29:00 +0600

    js add

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("my name is uzzwal")
+function showPopup (){
+    let box = document.getElementById("popupBox");
+
+    if(box.style.display =="none"){
+        box.style.display = "block";
+    }else{
+        box.style.display = "none"
+    }
+}
